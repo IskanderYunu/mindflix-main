4f71651506c99a8def9c3428dfafe8575dcde574
iskanderyunu <izzyyunu@gmail.com>
2025-01-21 23:47:35 -0500

    login updated code

--- mindflix-main/src/Pages/Login/Login.jsx
+++ mindflix-main/src/Pages/Login/Login.jsx
@@ -1,22 +1,62 @@
-import React from "react";
+//FINISH BACKEND LOGIN LOGIC
+import React, { useState } from "react";
+import axios from "axios";
 import "./Login.css";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/users/login",
+        formData
+      );
+      setMessage("User logged in successfully!");
+    } catch (error) {
+      setMessage("Error logging in user.");
+    }
+  };
+
   return (
     <div className="login-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" required />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
         </div>
         <button type="submit" className="login-button">
           Login
         </button>
+        {message && <p>{message}</p>}
       </form>
     </div>
   );
